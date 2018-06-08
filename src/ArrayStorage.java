323cde13166858fccc38c2eac2b7fd3bc731a270
Paul Lennik <paveltitan18@gmail.com>
2018-06-08 12:57:44 +0300

    My name id do

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
-/**
- * Array based storage for Resumes
- */
-public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+import java.util.Arrays; // Массивы
 
-    void clear() {
+public class ArrayStorage
+    {
+        Resume[] storage = new Resume[10000];
+            int size = 0;
+
+    void clear() { // Чистка
+        Arrays.fill(storage, 0, size, null);
+            size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume)
+    {
+        if (size < storage.length)
+        {
+            storage[size] = resume;
+                size++;
+        }
     }
 
-    Resume get(String uuid) {
+    Resume get(String uuid)
+    {
+        for (int i=0;i<size;i++){
+            if(storage[i].uuid==uuid)
+            {
+                return storage[i];
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    void delete(String uuid)
+    {
+
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
+        Resume[] getAll()
+        {
+            return new Resume[0]; //Возвращаем выражение Если не указано, то вместо него возвращается undefined.
     }
 
-    int size() {
-        return 0;
-    }
+                int size()
+                {
+                     return 0;
+                }
 }
