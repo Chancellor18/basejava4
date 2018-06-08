3e0d8bc3177d1163b9a4556358b7f38d303259bd
Paul Lennik <paveltitan18@gmail.com>
2018-06-08 14:11:06 +0300

    My name is do

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,61 +1,49 @@
 import java.util.Arrays; // Массивы
 
-public class ArrayStorage
-    {
-        Resume[] storage = new Resume[10000];
-            int size = 0;
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() { // Чистка
         Arrays.fill(storage, 0, size, null);
-            size = 0;
+        size = 0;
     }
 
-    void save(Resume resume)
-    {
-        if (size < storage.length)
-        {
+    void save(Resume resume) {
+        if (size < storage.length) {
             storage[size] = resume;
-                size++;
+            size++;
         }
     }
 
-    Resume get(String uuid)
-    {
-        for (int i=0;i<size;i++){
-            if(storage[i].uuid==uuid)
-            {
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid)
-    {
+    void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                    storage[i] = storage[size - 1];
-                        storage[size - 1] = null;
-                            size--;
-                                return;
-    }
-
-        Resume[] getAll()
-        {
-            return new Resume[0]; //Возвращаем выражение Если не указано, то вместо него возвращается undefined.
-    }
-
-                int size()
-                {
-                    return Arrays.copyOf(storage, size);
-                }
-
-            int size() {
-                return size;
-                }
-            }
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
 
             }
         }
+
+    }
+
+    Resume[] getAll(){
+        return Arrays.copyOf(storage,size);
+    }
+    int size() {
+        return size;
+    }
+}
 
 
